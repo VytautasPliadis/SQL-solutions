c720e875ed44ecae2c3bb1405a824198bbf786df
Vytautas Pliadis <vytautas.pliadis@gmail.com>
2023-01-29 08:05:19 +0200

    Update Chapter_4:_Filtering.sql

--- Chapter_4:_Filtering.sql
+++ Chapter_4:_Filtering.sql
@@ -5,3 +5,8 @@
 SELECT payment_id, customer_id, amount, date(payment_date)
 FROM payment
 WHERE amount IN (1.98, 7.98, 9.98);
+
+-- Exercise 4-3. Construct a query that finds all customers whose last name contains an A in the second position and a W anywhere after the A.
+SELECT fist_name, last_name
+FROM customer
+WHERE last_name ILIKE ('_A%W%') 

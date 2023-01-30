d9f5b18a4ec5172bbd26b5c1e9c12b42e3eab612
Vytautas Pliadis <vytautas.pliadis@gmail.com>
2023-01-30 08:37:30 +0200

    Update Chapter_6:_Working_with_Sets.sql

--- Chapter_6:_Working_with_Sets.sql
+++ Chapter_6:_Working_with_Sets.sql
@@ -8,6 +8,20 @@
 • A except B =    {L M N O }
 
 -- Exercise 6-2. Write a compound query that finds the first and last names of all actors and customers whose last name starts with L.
-
+SELECT a.first_name, a.last_name
+FROM actor a
+WHERE a.last_name LIKE 'L%'
+UNION
+SELECT c.first_name, c.last_name
+FROM customer c
+WHERE c.last_name LIKE 'L%';
 
 -- Exercise 6-3.Sort the results from Exercise 6-2 by the last_name column.
+SELECT a.first_name, a.last_name
+FROM actor a
+WHERE a.last_name LIKE 'L%'
+UNION
+SELECT c.first_name, c.last_name
+FROM customer c
+WHERE c.last_name LIKE 'L%'
+ORDER BY last_name;

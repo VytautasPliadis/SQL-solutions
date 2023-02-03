5b5fe458da36672b14bddf1e59c5f55c8d406b8a
Vytautas Pliadis <vytautas.pliadis@gmail.com>
2023-02-03 08:41:17 +0200

    Create Chapter_7:_Data_Generation,_Manipulation,_Conversion.sql

---
+++ Chapter_7:_Data_Generation,_Manipulation,_Conversion.sql
@@ -0,0 +1,4 @@
+-- Chapter 3 
+-- Data Generation, Manipulation,and Conversion
+
+-- Exercise 7-1. Write a query that returns the 17th through 25th characters of the string 'Please find the substring in this string'.

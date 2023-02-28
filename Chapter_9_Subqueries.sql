92d485a4aeac3b6105b60fcbc411bdbd1852df6f
Vytautas Pliadis <vytautas.pliadis@gmail.com>
2023-02-28 08:05:37 +0200

    Create Chapter_9_Subqueries.sql

---
+++ Chapter_9_Subqueries.sql
@@ -0,0 +1,6 @@
+-- Chapter 9 
+-- Subqueries
+
+-- Exercise 9-1. Construct a query against the film table that uses a filter condition with a noncorre‐
+lated subquery against the category table to find all action films (category.name =
+'Action').

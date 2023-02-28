efdfaa01bc39690583efbee979842ceff4645e51
Vytautas Pliadis <vytautas.pliadis@gmail.com>
2023-02-28 08:40:10 +0200

    Update Chapter_9_Subqueries.sql

--- Chapter_9_Subqueries.sql
+++ Chapter_9_Subqueries.sql
@@ -2,5 +2,12 @@
 -- Subqueries
 
 -- Exercise 9-1. Construct a query against the film table that uses a filter condition with a noncorre‐
-lated subquery against the category table to find all action films (category.name =
-'Action').
+-- lated subquery against the category table to find all action films (category.name = 'Action').
+SELECT title
+FROM film
+WHERE film_id IN
+    (SELECT fc.film_id
+     FROM film_category fc
+     INNER JOIN category c ON fc.category_id = c.category_id
+     WHERE c.NAME = 'Action')
+ORDER BY title;

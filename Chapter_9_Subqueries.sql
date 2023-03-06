680cc812a4e946da9efaef4f70b2d36395fcb8d0
Vytautas Pliadis <vytautas.pliadis@gmail.com>
2023-03-06 09:05:43 +0200

    Update Chapter_9_Subqueries.sql

--- Chapter_9_Subqueries.sql
+++ Chapter_9_Subqueries.sql
@@ -11,3 +11,32 @@ WHERE film_id IN
      INNER JOIN category c ON fc.category_id = c.category_id
      WHERE c.NAME = 'Action')
 ORDER BY title;
+
+-- Exercise 9-3
+-- Join the following query to a subquery against the film_actor table to show the level
+-- of each actor:
+-- SELECT 'Hollywood Star' level, 30 min_roles, 99999 max_roles
+-- UNION ALL
+-- SELECT 'Prolific Actor' level, 20 min_roles, 29 max_roles
+-- UNION ALL
+-- SELECT 'Newcomer' level, 1 min_roles, 19 max_roles
+-- The subquery against the film_actor table should count the number of rows for each
+-- actor using group by actor_id, and the count should be compared to the
+-- min_roles/max_roles columns to determine which level each actor belongs to.
+SELECT actr.actor_id,
+       grps.level
+FROM
+  (SELECT actor_id,
+          count(*) num_roles
+   FROM film_actor
+   GROUP BY actor_id) actr
+INNER JOIN
+  (SELECT 'Hollywood Star' LEVEL,
+                           30 min_roles,
+                           99999 max_roles
+   UNION ALL SELECT 'Prolific Actor' LEVEL,
+                                     20 min_roles,
+                                     29 max_roles
+   UNION ALL SELECT 'Newcomer' LEVEL,
+                               1 min_roles,
+                               19 max_roles) grps ON actr.num_roles BETWEEN grps.min_roles AND grps.max_roles;
